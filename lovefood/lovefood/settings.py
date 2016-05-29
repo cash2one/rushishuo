b6caf70e228dd5f4d670e587fa0a7e6c05402b87
Captain Rib <hjeffreydu@gmail.com>
2016-05-28 20:00:45 -0700

    Finished the overall design

--- lovefood/lovefood/settings.py
+++ lovefood/lovefood/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'lovefood.wsgi.application'
-
+SESSION_SERIALIZER='django.contrib.sessions.serializers.PickleSerializer'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
